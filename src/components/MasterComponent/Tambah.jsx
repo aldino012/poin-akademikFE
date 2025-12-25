b338ae91c5ade012bbcd3846b0b77d546314cc54
aldino012 <yozukakit@gmail.com>
2025-12-25 09:33:46 +0700

    update-login-mobile-fix

--- src/components/MasterComponent/Tambah.jsx
+++ src/components/MasterComponent/Tambah.jsx
@@ -211,7 +211,7 @@ export default function ModalTambahPoin({ isOpen, onClose, onSave }) {
               style={{ textTransform: "uppercase" }}
             />
             <p className="text-xs text-gray-500 mt-1">
-              4 karakter huruf kapital dan angka
+              3 karakter huruf kapital dan 1 angka
             </p>
           </div>
 
